16e688b6a953be1d012b2abb484ae8d12d28ef95
Louis Pierre Joseph Clergue <louis.pierre.joseph.clergue@a5s109pc13.fib.upc.es>
2016-09-16 11:15:15 +0200

    wip

--- newton.m
+++ newton.m
@@ -1,16 +1,17 @@
-function errors = newton(x, niter = 10, tol = 1e-16)
+function errors = newton(x0, niter = 10, tol = 1e-16)
   % usa el metode de newton per trobar un zero usant x d'aprox inicial
-  % [IN] x aproximacio inicial de la solucio
+  % [IN] x0 aproximacio inicial de la solucio
   errors = [];
   for k = 1:niter
-	x_new = x - eval_f(x)/eval_df(x);
-	r = abs ((x - x_new)/x_new);
-	errors = [ errors log(r) ];
-    printf("Iteration %d, solució %f, r = %f\n", k, x, r);
-	if (abs(x_new - x) < tol)
-		break;
-	endif
-	x = x_new;
+  dx = 1; % todo
+  x1 = x0 - eval_f(x0)/dx;
+  r = abs ((x0 - x1)/x1);
+  errors = [ errors r ];
+    printf("Iteration %d, solució %f, r = %f\n", k, x1, r);
+  if (abs(x1 - x0) < tol)
+    break;
+  endif
+  x0 = x1;
   endfor
-  printf("Solution %f\n", x);
+  printf("Solution %f\n", x1);
 endfunction
