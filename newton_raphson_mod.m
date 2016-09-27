445af2c67cbd510ad1015f3ed253a4490d94712c
Louis Clergue <louisclergue@gmail.com>
2016-09-27 23:01:22 +0200

    refactor newton_raphson_mod

--- newton_raphson_mod.m
+++ newton_raphson_mod.m
@@ -1,24 +1,28 @@
-function [errors, sol] = newton_raphson_mod(fname, jname, x0, niter = 10, tol = 1e-15)
-  % usa el metode de newton raphson per resolder un sistema eq no lineal amb aprox inicial
-  % [IN] fname nom de la funcio
-  % [IN] J aproximacio de la jacobiana
-  % [IN] x0 aproximacio inicial de la solucio
-  % [IN] niter nombre maxim d'iteracions
-  % [IN] tol tolerancia de l'error relatiu
+function [errors, sols] = newton_raphson_mod(x0, fname, jname, niter = 10, tol = 1e-15)
+  % usa el metode de newton raphson per resoldre un sistema eq no lineal amb aprox inicial
+  % [IN]  x0 aproximacio inicial de la solucio
+  % [IN]  fname nom de la funcio
+  % [IN]  J aproximacio de la jacobiana
+  % [IN]  niter nombre maxim d'iteracions
+  % [IN]  tol tolerancia de l'error relatiu
+  errors = []; sols = [];
   [L U] = lu(feval(jname, x0));
-  errors = [];
   for k = 1:niter
     dx = L\feval(fname, x0);
     dx = U\dx;
     x1 = x0 - dx;
     r = norm(x0 - x1)/norm(x1);
-    errors = [ errors log(r) ];
-    printf("Iteration %d, solució %s, r = %e\n", k, mat2str(x1), r);
+    errors = [ errors r];
+    sols = [ sols x1 ];
+    printf("IT%2d > sol = %s\tr = %e\n", k, mat2str(x1, 5), r);
+    x0 = x1;
     if (r < tol)
       break;
     endif
-    x0 = x1;
   endfor
-  sol = x1;
-  printf("Solution %s\n", mat2str(sol));
+  printf("Solution %s\n", mat2str(x0, 16));
 endfunction
+
+
+
+    
