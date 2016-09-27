5d2d8812d89a8163dcffef091dc3b31431c01054
Louis Clergue <louisclergue@gmail.com>
2016-09-27 22:24:03 +0200

    refactor newton

--- newton.m
+++ newton.m
@@ -1,17 +1,20 @@
-function errors = newton(x0, niter = 10, tol = 1e-16)
+function [errors, sols] = newton(x0, fname, dfname, niter = 10, tol = 1e-16)
   % usa el metode de newton per trobar un zero usant x d'aprox inicial
-  % [IN] x0 aproximacio inicial de la solucio
-  errors = [];
+  % [IN] x0     aproximacio inicial de la solucio
+  % [IN] fname  funcio que evalua f
+  % [IN] dfname funcio que evalua la derivada de f
+  % [IN] niter  nombre maxim d'iteracions
+  % [IN] tol    tolerancia sobre l'error relatiu 
+  errors = []; sols = [ x0 ];
   for k = 1:niter
-  dx = eval_df(x0); % todo
-  x1 = x0 - eval_f(x0)/dx;
-  r = abs ((x0 - x1)/x1);
-  errors = [ errors r ];
-    printf("Iteration %d, solució %f, r = %f\n", k, x1, r);
-  if (abs(x1 - x0) < tol)
-    break;
-  endif
-  x0 = x1;
+    x1 = x0 - feval(fname, x0)/feval(dfname, x0);
+    r = abs((x0 - x1)/x1);
+    errors = [ errors r ]; sols = [ sols x1 ];
+    printf("IT%d > sol = %f\tr = %e\n", k, x1, r);
+    if (r < tol)
+      break;
+    endif
+    x0 = x1;
   endfor
-  printf("Solution %f\n", x1);
+  printf("Solution %.16f\n", x1);
 endfunction
