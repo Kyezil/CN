f4a863309b95b5a680340e59dadba176f128985a
Louis Clergue <louisclergue@gmail.com>
2016-09-27 22:52:36 +0200

    reformat bisection

--- bisection.m
+++ bisection.m
@@ -1,30 +1,41 @@
-function errors = bisection(start, niter = 10, tol = 1e-15)
-  % usa el metode de la biseccio per trobar zeros en l'interval [a0,b0]
-  % [IN] start = (x0,a) interval de cerca inicial
+function [errors, sols] = bisection(start, fname, niter = 10, tol = 1e-15)
+  % usa el metode de la biseccio per trobar zeros en un interval
+  % [IN] start  interval inicial de cerca, de R2
+  % [IN] fname  funcio que evalua f
+  % [IN] niter  nombre maxim d'iteracions
+  % [IN] tol    tolerancia sobre l'error relatiu
   x0 = start(1); a = start(2);
-  s0 = eval_f(x0) >= 0;
-  errors = [];
-
+  s0 = feval(fname, x0) >= 0;
+  sA = feval(fname, a) >= 0;
+  errors = []; sols = [];
   for k = 1:niter
-    if (abs(x0 - a) < tol)
+    if (abs((x0 - a)/x0) < tol)
       break;
     endif
+    
+    if (a > x0)
+      [x0, a] = deal(a, x0);
+      [s0, sA] = deal(sA, s0);
+    endif
 
-    if (s0 && eval_f(a) >= 0)
+    if (s0 == sA)
       disp("error: interval no valid");
       return;
     endif
-
-    x1 = x0 + (a - x0)/2;
+    
+    x1 = x0 + (a - x0)/2; % punt mig
     r = abs(1 - x0/x1);
-    errors = [errors r];
-    printf("It %d: interval [%f, %f], r = %e\n", k, x0, a, r);
+    errors = [ errors r ];
+    sols = [ sols x1 ];
+  
+    printf("IT%2d > [%f, %f]\tr = %e\n", k, x0, a, r);
 
-    s1 = eval_f(x1) >= 0;
-    if (s1 != s0)
-      a = x0;
+    s1 = feval(fname, x1) >= 0;
+    if (s1 == s0)
+      x0 = x1; s0 = s1;
+    else
+      a = x1; sA = s1;
     endif
-    x0 = x1; s0 = s1;
   endfor
-  printf("Solution %f\n", x0);
+  printf("Solution %.16f\n", x0);
 endfunction
