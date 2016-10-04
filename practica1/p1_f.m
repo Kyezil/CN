253666a76260160ea225bc593dc39f2e819056b9
Louis Clergue <louisclergue@gmail.com>
2016-10-04 11:37:55 +0200

    random change i practica

---
+++ practica1/p1_f.m
@@ -0,0 +1,14 @@
+function par = par(t)
+  par = [2*cos(t) - cos(2*t), 2*sin(t) - sin(2*t)];
+endfunction
+
+function FT = f(t, p = [2.134141, 0.864141])
+  % evalua una funcio determinada
+  % [IN] t del domini de f
+  % [OUT] f(t)
+  X = 2*cos(t) - cos(2*t);
+  Y = 2*sin(t) - sin(2*t);
+  DX = -2*sin(t) + 2*sin(2*t);
+  DY = 2*cos(t) - 2*cos(2*t);
+  FT = (X - p(1))*DX + (Y - p(2))*DY;
+endfunction
