93cc86833280ec0296b8b7a67f1c6d4c7e33ad1b
Louis Clergue <louisclergue@gmail.com>
2016-10-24 13:46:47 +0200

    genera malla optima en tizx

--- practica1/problema3/calculaDistorsioMalla.m
+++ practica1/problema3/calculaDistorsioMalla.m
@@ -1,15 +1,18 @@
 function distorsioMalla = calculaDistorsioMalla(X,T)
 % ===> IMPLEMENTAR AQUI EL CALCUL DE LA DISTORSIO DE MALLA <===
+	A = [1  -sqrt(3)/3
+			0 2*sqrt(3)/3];
 
-A = [1  -sqrt(3)/3
-     0 2*sqrt(3)/3];
-
-numElements = size(T,1);
-for e = 1:numElements
-    % ===> IMPLEMENTAR AQUI EL CALCUL DE LA DISTORSIO DE L'ELEMENT e <===
-    
-    
-    
+	numElements = size(T,1);
+	distorsioMalla = 0;
+	for e = 1:numElements
+			% ===> IMPLEMENTAR AQUI EL CALCUL DE LA DISTORSIO DE L'ELEMENT e <===
+			xx = X(T(e,:),:); % (x1;x2;x3)
+			xx(2:end,:) -= xx(1,:); % (x1;x2-x1;x3-x1)
+			D = A*xx(2:end, :)';
+			nor = norm(D, 'fro')^2/(2*abs(det(D)));
+			distorsioMalla += nor^2;
+	end
+	distorsioMalla = sqrt(distorsioMalla);
 end
-
 
--- practica1/problema3/mainNewton.m
+++ practica1/problema3/mainNewton.m
@@ -1,10 +1,10 @@
 clear all; close all;
 load malla %X,T,Nint
-figure(1),plotMesh(X,T), title('Malla inicial') 
+figure(1),plotMesh(X,T), title('Malla inicial')
 
 % ===> IMPLEMENTAR la distorsio de malla a l'arxiu calculaDistorsioMalla.m <===
 F = @(y)(calculaDistorsioMalla([reshape(y,Nint,2);X(Nint+1:end,:)],T));
-fprintf('\nValor inicial de la distorsio: %2.6f\n',calculaDistorsioMalla(X,T))  
+fprintf('\nValor inicial de la distorsio: %2.6f\n',calculaDistorsioMalla(X,T))
 
 fprintf('\nNewton...\n')
 yk =  reshape(X(1:Nint,:),2*Nint,1);
@@ -13,10 +13,11 @@ while( convergence==0 && k<100)
     Fy = F(yk);
     DFy = numericalDerivative(F,yk); % avaluacio de la derivada numerica de F
     HFy = numericalHessian(F,yk);    % avaluacio de la Hessiana numerica de F
-    
+
     % ===> IMPLEMENTAR AQUI EL METODE DE NEWTON <===
-    
-    
+    dy = -HFy\DFy;
+    ykp1 = yk + dy;
+
     relErr(k+1) = norm(ykp1-yk)/norm(ykp1);
     convergence = relErr(k+1)<1.e-5;
     fprintf(' Iteracio %2d: Distorsio = %2.7f, Error relatiu y = %4.2e\n',k,Fy,relErr(k+1))
@@ -24,7 +25,9 @@ while( convergence==0 && k<100)
 end
 X(1:Nint,:) = reshape(yk,Nint,2);% es desfa el canvi de variable i s'actualitza la malla
 
-fprintf('\nValor final de la distorsio: %2.6f\n',calculaDistorsioMalla(X,T)) 
-figure(2),plotMesh(X,T), title('Malla optima') 
+fprintf('\nValor final de la distorsio: %2.6f\n',calculaDistorsioMalla(X,T))
+figure(2),plotMesh(X,T);
+axis off;
+%print(2, 'malla_optima.tex', '-dtikz');
 figure(3); plot(0:k-1,log10(relErr(1:k)),'-o')
 xlabel('k'), ylabel('log_{10}(r^k)')
