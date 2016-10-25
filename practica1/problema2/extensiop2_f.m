a5e6a89472e2d913e6d1a33e9d2200daefae021e
Louis Clergue <louisclergue@gmail.com>
2016-10-25 12:22:34 +0200

    Update extensiop2_f.m

--- practica1/problema2/extensiop2_f.m
+++ practica1/problema2/extensiop2_f.m
@@ -10,12 +10,12 @@ function FT3 = f3(t, k = 2)
 endfunction
 
 
-function JFT3 = df3(t, k1 = 2, k2 = 1.5) 
+function JFT3 = df3(t, k = 2) 
   % evalua numericament la jacobiana de la funcio de la extensio a tres varetes
   % [IN] t del domini de f3
   % [OUT] Jf3(t)
   t1 = t(1); t2 = t(2); t3 = t(3);
-  JFT3 = [k1*14/3*cos(t1)*cos(t1) - k1*sin(t1)*(14/3*sin(t1) + 3*sin(t2) + sin(t3)) - k2*5*cos(t1) ,k1*3*cos(t1)*cos(t2) ,k1*cos(t1)*cos(t3)
-  k1*3*cos(t1)*cos(t2), k1*8/3*cos(t2)*cos(t2) - k1*sin(t2)*(3*sin(t1) + 8/3*sin(t2) + sin(t3)) - k2*3*cos(t2), k1*cos(t2)*cos(t3)
-   k1*cos(t1)*cos(t3), k1*cos(t2)*cos(t3), k1*2/3*cos(t3)*cos(t3) - k1*sin(t3)*(sin(t1) + sin(t2) + 2/3*sin(t3)) - k2*cos(t3)];
+  JFT3 = [k*14/3*cos(t1)*cos(t1) - k*sin(t1)*(14/3*sin(t1) + 3*sin(t2) + sin(t3)) - 5*cos(t1) ,k*3*cos(t1)*cos(t2) ,k*cos(t1)*cos(t3)
+  k*3*cos(t1)*cos(t2), k*8/3*cos(t2)*cos(t2) - k*sin(t2)*(3*sin(t1) + 8/3*sin(t2) + sin(t3)) - 3*cos(t2), k*cos(t2)*cos(t3)
+   k*cos(t1)*cos(t3), k*cos(t2)*cos(t3), k*2/3*cos(t3)*cos(t3) - k*sin(t3)*(sin(t1) + sin(t2) + 2/3*sin(t3)) - cos(t3)];
 endfunction
