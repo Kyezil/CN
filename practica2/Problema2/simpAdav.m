6b860c2d64ad3a1a4cd9426b5dc047a9ac5f5a30
Louis Clergue <louisclergue@gmail.com>
2016-12-08 12:19:47 +0100

    improve simpsons adaptive

--- practica2/Problema2/simpAdav.m
+++ practica2/Problema2/simpAdav.m
@@ -1,21 +1,38 @@
 %Modificacio de la quadratura de Simpson adaptativa per retornar la divisio de [a, b] resultant
-function [res, x] = simpAdav(f, a, b, tol) 
-  [res, x] = simpAdav2(f, a, b, tol/(b - a));
-  x = [a x b];
+function [res, x] = simpAdav(f, a, b, tol)
+  c = (a+b)/2;
+  fa = f(a); fb = f(b); fc = f(c);
+  s = ((b-a)/6) * (fa + 4*fc + fb);
+  [res, x] = r_simpAdav(f, [a,c,b], [fa,fc,fb], s, tol/(b - a));
 end
 
-function  [res, x] = simpAdav2(f, a, b, tol) 
-  x = [];
-  mid = (a + b)/2;
-  res1 = integral_newton(f, a, mid, 3, 0)(1);
-  res2 = integral_newton(f, mid, b, 3, 0)(1);
-  res = res1 + res2;
-  s = integral_newton(f, a, b, 3, res)(1);
-  err = abs(s - res);
+function  [res, X] = r_simpAdav(f, x, fx, s, tol) 
+% f     function to integrate
+% x     [a,c,b] points where function is valued 
+% fx    [fa,fc,fb] value of function in x
+% s     value of the integral of f in [a,b]
+% tol   max error wanted
+  a = x(1); c = x(2); b = x(3);
+  fa = fx(1); fc = fx(2); fb = fx(3);
+  h = (b-a)/6; % interval
+ 
+  % quad in [a c]
+  d = (a+c)/2; fd = f(d);
+  sleft = (h/2)*(fa + 4*fd + fc);
+  % quad in [c b]
+  e = (c+b)/2; fe = f(e);
+  sright = (h/2)*(fc + 4*fe + fb);
+  % quad in [a b]
+  s2 = sleft + sright;
+  
+  err = abs(s - s2);
   if err >= tol*(b - a)
-    [res1, x1] = simpAdav2(f, a, mid, tol);
-    [res2, x2] = simpAdav2(f, mid, b, tol);
-    x = [x1 mid x2];
+    [res1, x1] = r_simpAdav(f, [a,d,c], [fa,fd,fc], sleft, tol);
+    [res2, x2] = r_simpAdav(f, [c,e,b], [fc,fe,fb], sright, tol);
+    X = [x1, x2(2:end)];
     res = res1 + res2;
-  endif
+  else
+    X = [a b];
+    res = s;
+  end
 end
